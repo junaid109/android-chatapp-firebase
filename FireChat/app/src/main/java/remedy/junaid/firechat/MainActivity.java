e43434304d32b319b168cc1f94447361b1aa3505
Junaid Malik <junaid-109@hotmail.com>
2018-08-12 15:59:05 +0100

    empty2

---
+++ FireChat/app/src/main/java/remedy/junaid/firechat/MainActivity.java
@@ -0,0 +1,13 @@
+package remedy.junaid.firechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
